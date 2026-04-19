cb072a57957daa42fa72b24499b8e1872d79b57a
collinsgichuki11-byte <collinsgichuki11@gmail.com>
2026-04-19 11:45:36 +0300

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE='cheka-v1';
+const ASSETS=[
+  '/',
+  '/index.html',
+  '/feed.html',
+  '/login.html',
+  '/signup.html',
+  '/profile.html',
+  '/creators.html',
+  '/notifications.html',
+  '/style.css'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
